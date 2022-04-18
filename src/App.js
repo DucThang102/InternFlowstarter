f4d6e5a0e2168c137549fd6dcad53e40f6ef316a
huavanduy <huavanduy1106@gmail.com>
2022-04-18 16:51:41 +0700

    (add): add responsive and logic pagination

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import {
   Spin,
   Modal,
   Input,
+  Pagination,
+  Tooltip,
 } from "antd";
 import { ethers } from "ethers";
 import React from "react";
@@ -51,10 +53,17 @@ function App() {
   const [heroes, setHeroes] = React.useState([]);
   const [loading, setLoading] = React.useState(false);
   const [visibleModal, setVisibleModal] = React.useState(false);
-  const [address, setAdress] = React.useState("");
+  const [address, setAddress] = React.useState("");
   const [selectedHero, setSelectedHero] = React.useState({});
   const [visibleTransfer, setVisibleTransfer] = React.useState(true);
   const [fileUrl, setFileUrl] = React.useState("");
+  const [messErr, setMessErr] = React.useState("");
+  const [active, setActive] = React.useState(0);
+  const [provider, setProvider] = React.useState("");
+  const [currentPage, setCurrentPage] = React.useState(1);
+  const [pageSize, setPageSize] = React.useState(10);
+  const [dataRender, setDataRender] = React.useState([]);
+  const [totalRecord, setTotalRecord] = React.useState(0);
 
   const renderSelect = (type) => {
     return dropDown[type].map((item) => {
@@ -67,40 +76,61 @@ function App() {
   };
 
   const handleUploadImg = async (e) => {
+    const inputFile = document.querySelector(".img_file");
     const client = create("https://ipfs.infura.io:5001/api/v0");
     const file = e.target.files[0];
-    try {
-      const added = await client.add(file);
-      const url = `https://ipfs.infura.io/ipfs/${added.path}`;
-      setFileUrl(url);
-      console.log("upload");
-    } catch (error) {
-      console.log("Error uploading file: ", error);
+    const maxSize = 1000000;
+    const { type, size } = file;
+    if (type === "image/png" || type === "image/jpeg" || type === "image/jpg") {
+      setMessErr("");
+      if (size <= maxSize) {
+        try {
+          const added = await client.add(file);
+          const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+          setFileUrl(url);
+        } catch (error) {
+          setMessErr("Error uploading file: ", error);
+        }
+      } else {
+        setMessErr("Maximum size is 1MB");
+      }
+    } else {
+      setMessErr("Format not supported");
+      inputFile.value = "";
     }
   };
 
   const handleCreateHero = async (values) => {
     setLoading(true);
-    if (signer !== "") {
-      const contract = new ethers.Contract(CONTRACT_ADDRESS, abi.abi, signer);
-      try {
-        const response = await contract.createHero(
-          fileUrl,
-          values.class,
-          values.sex,
-          values.generation,
-          values.star
-        );
-        const res = await response.wait();
-        if (res.status === 1) {
-          notification.success({ message: "Create Hero Successfully" });
-          getAllHeroes();
-        } else {
-          notification.error({ message: "Create Hero Error" });
+    if (signer) {
+      if (fileUrl) {
+        const contract = new ethers.Contract(CONTRACT_ADDRESS, abi.abi, signer);
+        try {
+          const response = await contract.createHero(
+            fileUrl,
+            values.class,
+            values.sex,
+            values.generation,
+            values.star
+          );
+          const res = await response.wait();
+          getHeroesOfAccount();
+          setLoading(false);
+          if (res.status === 1) {
+            notification.success({ message: "Create Hero Successfully" });
+          } else {
+            notification.error({ message: "An error occurred" });
+          }
+        } catch (error) {
+          notification.error({ message: error.message });
+          setLoading(false);
         }
-      } catch (error) {
-        console.log(error);
+      } else {
+        setMessErr("Select Image For NFT");
+        setLoading(false);
       }
+    } else {
+      notification.error({ message: "You are not connected to metamask" });
       setLoading(false);
     }
   };
@@ -126,39 +156,62 @@ function App() {
 
   const connectMetamask = async () => {
     setLoading(true);
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    await provider.send("eth_requestAccounts", []);
-    const signer = provider.getSigner();
-    const account = await signer.getAddress();
-    setAccount(account);
-    setSigner(signer);
-    setConnected(true);
-    setLoading(false);
+    try {
+      await provider.send("eth_requestAccounts", []);
+      const signer = provider.getSigner();
+      const account = await signer.getAddress();
+      setAccount(account);
+      setSigner(signer);
+      setConnected(true);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      notification.error({ message: "MetaMask is not installed" });
+    }
   };
 
   const getAllHeroes = async () => {
-    setLoading(true);
-    if (signer !== "") {
-      const contract = new ethers.Contract(CONTRACT_ADDRESS, abi.abi, signer);
-      const response = await contract.getAllHeroes();
-      setHeroes(response);
+    if (provider) {
+      setLoading(true);
+      try {
+        const contract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          abi.abi,
+          provider
+        );
+        const response = await contract.getAllHeroes();
+        setTotalRecord(response.length);
+        setHeroes(response);
+      } catch (error) {
+        notification.error({ message: error.message });
+        setLoading(false);
+      }
+      setLoading(false);
     }
-    setLoading(false);
   };
 
   const getHeroesOfAccount = async () => {
     setLoading(true);
-    if (signer !== "") {
-      const contract = new ethers.Contract(CONTRACT_ADDRESS, abi.abi, signer);
-      const response = await contract.getHeroesOfAccount();
-      setHeroes(response);
+    if (signer) {
+      try {
+        const contract = new ethers.Contract(CONTRACT_ADDRESS, abi.abi, signer);
+        const response = await contract.getHeroesOfAccount();
+        setHeroes(response);
+        setTotalRecord(response.length);
+      } catch (error) {
+        notification.error({ message: error.message });
+        setLoading(false);
+      }
+    } else {
+      notification.error({ message: "You are not connected to metamask" });
+      setLoading(false);
     }
     setLoading(false);
   };
 
   const handleTranfer = async () => {
     setLoading(true);
-    if (signer !== "") {
+    if (signer) {
       const contract = new ethers.Contract(CONTRACT_ADDRESS, abi.abi, signer);
       try {
         const response = await contract.transferHero(
@@ -167,106 +220,175 @@ function App() {
           selectedHero.id
         );
         const res = await response.wait();
-        if (res.status === 1) {
+        const { status } = res;
+        if (status === 1) {
           notification.success({ message: "Transfer this hero successfully" });
+          getHeroesOfAccount();
         } else {
-          notification.error({ message: "Transfer this hero error" });
+          notification.error({ message: "An error occurred" });
         }
-        getAllHeroes();
       } catch (error) {
-        notification.error({ message: "Transfer this hero error" });
+        notification.error({ message: error.message });
+        setLoading(false);
       }
       setLoading(false);
       setVisibleModal(false);
-      setAdress("");
+      setAddress("");
+    } else {
+      notification.error({ message: "You are not connected to metamask" });
+      setLoading(false);
     }
   };
+
+  const onChangePage = (page = 1, pageSize = 5) => {
+    setCurrentPage(page);
+    setPageSize(pageSize);
+  };
+
+  React.useEffect(() => {
+    if (typeof window.ethereum !== "undefined") {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      setProvider(provider);
+    } else {
+      notification.error({ message: "MetaMask is not installed" });
+    }
+    // eslint-disable-next-line
+  }, []);
 
   React.useEffect(() => {
     getAllHeroes();
     // eslint-disable-next-line
-  }, [signer]);
+  }, [provider]);
 
   React.useEffect(() => {
-    connectMetamask();
-    // eslint-disable-next-line
-  }, []);
-
+    const minValue = (currentPage - 1) * pageSize;
+    const maxValue = currentPage * pageSize;
+    const newData = [];
+    heroes.map((item, index) => {
+      if (index >= minValue && index < maxValue) {
+        newData.push(item);
+      }
+    });
+    setDataRender(newData);
+  }, [currentPage, pageSize, heroes]);
   return (
     <div className="App">
       <Spin spinning={loading} tip="Loading...">
         <div className="heroes">
           <Row>
-            <Col span={8}></Col>
-            <Col span={12}>
+            <Col xl={8} md={0} xs={0}></Col>
+            <Col xl={12} md={12} xs={24}>
               <Button
+                style={{ marginRight: "10px" }}
+                type={active === 0 ? "primary" : ""}
                 onClick={() => {
                   getAllHeroes();
+                  setActive(0);
+                  setCurrentPage(1);
                   setVisibleTransfer(true);
                 }}
               >
                 All Heroes
               </Button>
               <Button
+                disabled={signer === ""}
+                type={active === 1 ? "primary" : ""}
                 onClick={() => {
                   getHeroesOfAccount();
+                  setActive(1);
+                  setCurrentPage(1);
                   setVisibleTransfer(false);
                 }}
               >
                 My Heroes
               </Button>
             </Col>
-            <Col span={4}>
-              <Button
-                disabled={connected}
-                style={{ float: "right" }}
-                onClick={() => connectMetamask()}
-              >
-                {!connected ? "Connect Metamask" : account}
-              </Button>
+            <Col xl={4} md={12} xs={24}>
+              <Tooltip placement="top" title={account}>
+                <Button
+                  className="info-signer"
+                  disabled={connected}
+                  onClick={() => connectMetamask()}
+                >
+                  {!connected ? "Connect Metamask" : account}
+                </Button>
+              </Tooltip>
             </Col>
           </Row>
-          <Row>
-            <Col span={8}>
+          <Row style={{ paddingTop: "20px" }}>
+            <Col xl={8} md={24} xs={24}>
               <Form
-                labelCol={{ span: 4 }}
-                wrapperCol={{ span: 14 }}
                 layout="vertical"
+                initialValues={{
+                  class: 0,
+                  sex: 0,
+                  generation: 0,
+                  star: 0,
+                }}
                 onFinish={handleCreateHero}
               >
-                <Form.Item label="Avatar">
-                  <input type="file" onChange={handleUploadImg} />
+                <Form.Item
+                  label="Avatar"
+                  extra={
+                    <div>
+                      <div>JPG/JPEG/PNG </div>
+                      <div> Maximum: 1MB</div>
+                    </div>
+                  }
+                >
+                  <input
+                    type="file"
+                    className="img_file"
+                    onChange={handleUploadImg}
+                    accept="image/*"
+                  />
+                  <div style={{ color: "red" }}> {messErr} </div>
                 </Form.Item>
-                <Form.Item name="class" label="Class">
-                  <Select>{renderSelect("heroClass")}</Select>
-                </Form.Item>
-                <Form.Item name="sex" label="Sex">
-                  <Select>{renderSelect("sex")}</Select>
-                </Form.Item>
-                <Form.Item name="generation" label="Generation">
-                  <Select>{renderSelect("generation")}</Select>
-                </Form.Item>
-                <Form.Item name="star" label="Star">
-                  <Select>{renderSelect("star")}</Select>
-                </Form.Item>
+                <Row gutter={64}>
+                  <Col xl={20} md={12} xs={24}>
+                    <Form.Item name="class" label="Class">
+                      <Select>{renderSelect("heroClass")}</Select>
+                    </Form.Item>
+                  </Col>
+                  <Col xl={20} md={12} xs={24}>
+                    <Form.Item name="sex" label="Sex">
+                      <Select>{renderSelect("sex")}</Select>
+                    </Form.Item>
+                  </Col>
+                  <Col xl={20} md={12} xs={24}>
+                    <Form.Item name="generation" label="Generation">
+                      <Select>{renderSelect("generation")}</Select>
+                    </Form.Item>
+                  </Col>
+                  <Col xl={20} md={12} xs={24}>
+                    <Form.Item name="star" label="Star">
+                      <Select>{renderSelect("star")}</Select>
+                    </Form.Item>
+                  </Col>
+                </Row>
                 <Button type="default" htmlType="submit">
                   Create Hero
                 </Button>
               </Form>
             </Col>
-            <Col span={16}>
-              <Row style={{ paddingTop: "16px" }}>
-                {heroes.map((item, index) => {
+            <Col xl={16} md={24}>
+              <Row gutter={64} style={{ paddingTop: "16px" }}>
+                {dataRender.map((item, index) => {
                   return (
-                    <Col style={{ paddingTop: "10px" }} span={8} key={index}>
+                    <Col
+                      style={{ paddingTop: "10px" }}
+                      xxl={8}
+                      xl={12}
+                      md={12}
+                      xs={24}
+                      key={index}
+                    >
                       <Card
                         hoverable
-                        style={{ width: 240 }}
                         cover={
                           <img
                             alt="example"
                             style={{
-                              width: "240px",
                               height: "240px",
                               objectFit: "cover",
                             }}
@@ -294,6 +416,20 @@ function App() {
                   );
                 })}
               </Row>
+              {totalRecord ? (
+                <Pagination
+                  style={{ float: "right", margin: "30px 0" }}
+                  onChange={onChangePage}
+                  defaultPageSize={10}
+                  defaultCurrent={1}
+                  total={totalRecord}
+                  showTotal={(total) => {
+                    return `Total ${total} items`;
+                  }}
+                />
+              ) : (
+                ""
+              )}
             </Col>
           </Row>
         </div>
@@ -308,7 +444,7 @@ function App() {
         >
           <Input
             value={address}
-            onChange={(e) => setAdress(e.target.value)}
+            onChange={(e) => setAddress(e.target.value)}
             placeholder="Receiver's address"
           />
         </Modal>
