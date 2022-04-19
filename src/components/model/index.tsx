8896915dc67dc1e6dae1d97f1aeecee03c277958
Van Trong Hung 20173158 <hung.vt173158@sis.hust.edu.vn>
2022-04-19 09:44:45 +0700

    update

--- src/components/model/index.tsx
+++ src/components/model/index.tsx
@@ -12,7 +12,7 @@ const Modal = (props: propsModal) => {
     const [showElemamt, setShowElement] = useState(show);
 
     const escFunction = useCallback(
-        (event) => {
+        (event: any) => {
             if (event.key === "Escape") {
                 onClose();
             }
--- src/router/Routers.tsx
+++ src/router/Routers.tsx
@@ -1,14 +1,15 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import route from "./route";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import route from "./route";
 import Home from "../screen/home";
 
 const Routers = () => {
     return (
-        <Router>
-            <Switch>
-                <Route path={route.HOME} component={Home} />
-            </Switch>
-        </Router>
+        <Home />
+        // <Router>
+        //     <Switch>
+        //         <Route path={route.HOME} component={Home} />
+        //     </Switch>
+        // </Router>
     );
 };
 export default Routers;
